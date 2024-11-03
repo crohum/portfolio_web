aa53d3d8fcbe0b2233839f271291646514595354
Crohum <crohum@hotmail.com>
2024-11-03 11:00:10 -0600

    add components

---
+++ portfolio_web/componentes/enlaces.py
@@ -0,0 +1,11 @@
+# REDES:
+LINKEDIN_URL = 'https://github.com/crohum'
+GITHUB_URL = 'https://github.com/crohum'
+
+
+# ENLACE IMPORTANTE
+ENLACE = 'https://reflex.dev/docs/library/typography/text/'
+
+
+# ENLACE A LA PAGINA
+PORTFOLIO_URL = 'https://github.com/crohum/portfolio_web'
